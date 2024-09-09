4f9c7859e400ac19e973ebf9b9d8bd5176b58866
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-09-09 10:03:46 -0500

    Changed structure of code to use a class instead of struct as c++ style is to use classes

--- program.cpp
+++ program.cpp
@@ -3,27 +3,55 @@
 
 using namespace std;
 
-struct Test {
-	vector<int> a;
+class Test {
+	private:
+		vector<int> a;
+	public:
+		// Constructor
+		Test();
+
+		// Facilitator
+		void changeArr();
+
+		// Setter
+		void set_vector(int num) {
+			a.push_back(num);
+		}
+
+		// Getter
+		int get_vector(int num) {
+			return a.at(num);
+		}
+
+		// Destructor
+		~Test();
 };
 
-void changeArr(struct Test *t1) {
-	for (int i = 0; i < 22; i++) {
-		t1->a.at(i)++;
+Test::Test() {
+	cout << "Constructor Called" << endl;
+}
+
+void Test::changeArr() {
+	for (int i = 0; i < 20; i++) {
+		a.at(i)++;
 	}
 }
 
+Test::~Test() {
+	cout << "Destructor Called" << endl;
+}
+
 int main() {
-    struct Test t;
+    Test t;
     	
-    for (int i = 0; i < 22; i++) {
-    	t.a.push_back(i + 1);
-    	cout << t.a.at(i) << endl;
+    for (int i = 0; i < 20; i++) {
+    	t.set_vector(i + 1);
+    	cout << t.get_vector(i) << endl;
     }
     
-    changeArr(&t);
+    t.changeArr();
     
-    for (int i = 0; i < 22; i++) {
-    	cout << t.a.at(i) << endl;
+    for (int i = 0; i < 20; i++) {
+    	cout << t.get_vector(i) << endl;
     }
 }
